aaf1a35f983c2637d9bcdfeb4f7797cb6433929a
Okedo01 <eliahmwelangi01@gmail.com>
2025-05-14 14:08:35 +0300

    todo-project

--- src/components/Trial.jsx
+++ src/components/Trial.jsx
@@ -6,15 +6,16 @@ const Trial = () => {
 
 const handleAdd = () => {
     if(newTodo.trim() !== "") {
-        setTodos([...todos, {text: newTodo.trim}]);
+        setTodos([...todos, {text: newTodo.trim()}]);
         setNewTodo("");
     }
 }
 
-// const handleToggle = (index) => {
-//     const toggleTodo = setTodos([...todos]);
-//     toggleTodo(index).checked = !toggleTodo(index).checked;
-// }
+const handleDelete = (index) => {
+    console.log("deleted", index);
+    const deletedTodos = todos.filter((_, i) => index !== i);
+    setTodos(deletedTodos);
+}
   return (
     <>
         <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
@@ -22,73 +23,15 @@ const handleAdd = () => {
 
         <ul>
             {todos.map((todo, index) => (
-                <p key={index}>
-                    {todo.text}
-                </p>
-            ))}
-        </ul>
-
-        {/* <ul>
-            {todos.map((todo, index) => (
-                <li>
-                    <input type="checkbox" checked={todo.checked} onChange={() => handleToggle(index)} />
+                <li key={index}>
+                    <input type="checkbox" />
                     <span>{todo.text}</span>
-                    <button onClick={handleDelete}>Delete</button>
+                    <button onClick={() => handleDelete(index)}>Delete</button>
                 </li>
             ))}
-        </ul> */}
+        </ul>
     </>
   )
 }
 
 export default Trial
-
-
-// import React, { useState } from 'react';
-
-// const Trial = () => {
-//     const [newTodo, setNewTodo] = useState("");
-//     const [todos, setTodos] = useState([]);
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         if (newTodo.trim() !== "") {
-//             setTodos([...todos, { text: newTodo.trim(), checked: false }]);
-//             setNewTodo("");
-//         }
-//     };
-
-//     const handleToggle = (index) => {
-//         const updatedTodos = todos.map((todo, i) =>
-//             i === index ? { ...todo, checked: !todo.checked } : todo
-//         );
-//         setTodos(updatedTodos);
-//     };
-
-//     return (
-//         <>
-//             <form onSubmit={handleSubmit}>
-//                 <input
-//                     type="text"
-//                     value={newTodo}
-//                     onChange={(e) => setNewTodo(e.target.value)}
-//                 />
-//                 <button type="submit">Add</button>
-//             </form>
-//             <ul>
-//                 {todos.map((todo, index) => (
-//                     <li key={index}>
-//                         <input
-//                             type="checkbox"
-//                             checked={todo.checked}
-//                             onChange={() => handleToggle(index)}
-//                         />
-//                         {todo.text}
-//                     </li>
-//                 ))}
-//             </ul>
-//         </>
-//     );
-// };
-
-// export default Trial;
